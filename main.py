b035c72f73ed661bfae156154d4dc64f21808db7
Alex <alexbs.ga@gmail.com>
2023-11-11 20:41:42 +0300

    better search

--- main.py
+++ main.py
@@ -15,22 +15,29 @@ def get_random_string():
     
 # Find in json by string
 @app.get("/search/{string}")
-def get_by_string(string):
+def get_by_string(string: str, randomize: bool = False, limit: int = 10, offset: int = 0):
     results = []
     with open("./aneks.json", "r", encoding='utf-8') as file:
         data = json.load(file)
-        for obj in data:
-            if string in obj["text"]:
-                results.append(obj["text"])
-    return JSONResponse(content=results)
+        for i in range(len(data)):
+            obj = data[i]
+            if string in obj:
+                results.append(obj)
+    if(randomize):
+        random.shuffle(results)
+ 
+    return JSONResponse(content=results[:limit][offset:])
 
 # Find in json exclusive by string
 @app.get("/search_exclusive/{string}")
-def get_by_string_exclusive(string):
+def get_by_string_exclusive(string: str, randomize: bool = False, limit: int = 10, offset: int = 0):
     results = []
     with open("./aneks.json", "r", encoding='utf-8') as file:
         data = json.load(file)
-        for obj in data:
-            if string in obj["text"] and obj["text"] not in results:
-                results.append(obj["text"])
-    return JSONResponse(content=results)
+        for i in range(len(data)):
+            obj = data[i]
+            if string not in obj:
+                results.append(obj)
+    if(randomize):
+        random.shuffle(results)
+    return JSONResponse(content=results[:limit][offset:])
